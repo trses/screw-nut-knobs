979c851d2a84002632a35c93d9052b6f1954ae70
Thomas Richter <mail@thomas-richter.info>
2024-12-21 16:16:02 +0100

    renamed files, quality option -q for the script

--- create_knobs.sh
+++ create_knobs.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-sizes=("M4" "M5") # "M6" "M8")
+quality=12
+
+# see if we have a quality argument -q
+while getopts q: flag
+do
+    case "${flag}" in
+        q) quality=${OPTARG};;
+    esac
+done
+
+sizes=("M4" "M5" "M6" "M8")
 arms=(5 7)
 types=("nut" "nutx" "screw" "screwx")
 
@@ -9,18 +19,18 @@ for size in "${sizes[@]}"; do
         for arm in "${arms[@]}"; do
             /Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD \
              --D TYPE=\"$type\" --D SIZE=\"$size\" --D ARMS=$arm \
-             --D QUALITY=12 \
+             --D QUALITY=$quality \
              --export-format binstl \
              --o ./printables/$size-$type-$arm-arms.stl \
-             --backend=manifold schraubenkopf-round.scad
+             --backend=manifold screw-nut-knobs.scad
         done
     done
 
     # hubs don't come with arms
     /Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD \
         --D TYPE=\"hub\" --D SIZE=\"$size\" \
-        --D QUALITY=12 \
+        --D QUALITY=$quality \
         --export-format binstl \
         --o ./printables/$size-hub.stl \
-        --backend=manifold schraubenkopf-round.scad
+        --backend=manifold screw-nut-knobs.scad
 done

ee02f4ed8a2ad67a19465662b358e319ba5f2570
Thomas Richter <mail@thomas-richter.info>
2024-12-22 21:39:41 +0100

    added comments to the bash script

--- createKnobs.sh
+++ createKnobs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# I use this script to create the different knobs all at once.
+# Works with OpenSCAD on Macs. For command line usage on Linux and Windows
+# please refer to
+# https://en.wikibooks.org/wiki/OpenSCAD_User_Manual/Using_OpenSCAD_in_a_command_line_environment
+
 quality=12
 
 # see if we have a quality argument -q
@@ -12,14 +17,17 @@ done
 
 sizes=("M4" "M5" "M6" "M8")
 arms=(5 7)
+# nut, nutx, screw, screwx, allen, hub
 types=("allen")
+# flat, rounded
+shape="rounded"
 
 for size in "${sizes[@]}"; do
     for type in "${types[@]}"; do
         for arm in "${arms[@]}"; do
             /Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD \
              --D TYPE=\"$type\" --D SIZE=\"$size\" --D ARMS=$arm \
-             --D SHAPE=\"rounded\" \
+             --D SHAPE=\"$shape\" \
              --D QUALITY=$quality \
              --export-format binstl \
              --o ./printables/$size-$type-$arm-arms.stl \
